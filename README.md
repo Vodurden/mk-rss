0ba72cdc5ba85f80a8dee6890d4588ab0b74a823
Jake Woods <jake@jakewoods.net>
2020-08-14 11:58:57 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Releasing
+
+1. Build `mk-rss` statically: `cargo build --target x86_64-unknown-linux-musl`
+2. TODO
---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  sources = import nix/sources.nix;
+  pkgs = import sources.nixpkgs { overlays = [(import sources.nixpkgs-mozilla)]; };
+  unstable = import sources.nixpkgs-unstable {};
+
+  rustChannel = pkgs.rustChannelOfTargets "stable" null [
+    "x86_64-unknown-linux-musl"
+  ];
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    rustChannel
+
+    rustfmt
+    unstable.rust-analyzer
+  ];
+}
